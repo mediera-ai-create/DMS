2492e6637ca88fc9a069fa257ad69e95420571a9
Santosh Moharir <moharirsantoshkumar@gmail.com>
2025-08-11 17:37:44 +0530

    Dimension Api

---
+++ DMS.Api/Controllers/DimensionController.cs
@@ -0,0 +1,27 @@
+﻿using DMS.Application.DTOs;
+using DMS.Application.Interfaces;
+using DMS.Application.Services.Pdf;
+using DMS.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DimensionController : ControllerBase
+{
+    private readonly IDimensionService _svc;
+    public DimensionController(IDimensionService svc) => _svc = svc;
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] DimensionDto dto) => Ok(await _svc.AddAsync(dto));
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => Ok(await _svc.GetAllAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var d = await _svc.GetByIdAsync(id);
+        return d == null ? NotFound() : Ok(d);
+    }
+}
